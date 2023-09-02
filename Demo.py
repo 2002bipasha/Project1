f2f4304f794ca7500d518a46836baab315522586
2002bipasha <143878061+2002bipasha@users.noreply.github.com>
2023-09-02 16:11:15 +0530

    Create Demo.py

---
+++ Demo.py
@@ -0,0 +1,20 @@
+def fibo(n):
+  a = 0
+  b = 1
+  if n < 0:
+    print("Invalid Input")
+  elif n == 0:
+    print(a)
+  elif n == 1:
+    print(b)
+  else:
+    print(a, b, end=" ")
+    for i in range(2, n):
+      c = a + b
+      print(c, end=" ")
+      a = b
+      b = c
+print("Fibonacci Series Program : ")
+n = int(input("Enter the number of terms you want to print : "))
+fibo(n)
+print()
